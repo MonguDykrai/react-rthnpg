f914a0a6a256d5e62ff6673c5a4585ed179b5a5d
MonguDykrai <kisaihou@gmail.com>
2022-07-01 15:39:32 +0800

    1 file modified, 1 added

--- src/Downloader.2.js
+++ src/Downloader.2.js
@@ -20,6 +20,47 @@ export default function Downloader() {
     return textFile;
   };
 
+  // useEffect(() => {
+  //   (function () {
+  //     var textFile = null,
+  //       makeTextFile = function (text) {
+  //         var data = new Blob([text], { type: 'text/plain' });
+
+  //         // If we are replacing a previously generated file we need to
+  //         // manually revoke the object URL to avoid memory leaks.
+  //         if (textFile !== null) {
+  //           window.URL.revokeObjectURL(textFile);
+  //         }
+
+  //         textFile = window.URL.createObjectURL(data);
+
+  //         return textFile;
+  //       };
+
+  //     // var create = document.getElementById('create'),
+  //     // textbox = document.getElementById('textbox');
+
+  //     // create.addEventListener(
+  //     //   'click',
+  //     //   function () {
+  //     //     var link = document.getElementById('downloadlink');
+  //     //     link.href = makeTextFile(textbox.value);
+  //     //     link.style.display = 'block';
+  //     //   },
+  //     //   false
+  //     // );
+
+  //     // create.addEventListener(
+  //     //   'click',
+  //     //   function () {
+  //     //     var link = document.getElementById('downloadlink');
+  //     //     link.href = makeTextFile(textbox.value);
+  //     //     link.click();
+  //     //   },
+  //     //   false
+  //     // );
+  //   })();
+  // }, []);
   return (
     <div>
       {/* <textarea id="textbox">Type something here</textarea> */}
